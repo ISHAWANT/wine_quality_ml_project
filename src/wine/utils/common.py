fb17b853362a575a4a4915b39c8ef6ff500995be
ishawant kumar <ishawantkumar@gmail.com>
2026-04-02 13:46:47 +0530

    helper function updated

--- src/wine/utils/common.py
+++ src/wine/utils/common.py
@@ -1,15 +1,13 @@
-import os
-from box.exceptions import BoxValueError
-import yaml
-from mlProject import logger
 import json
 import joblib
 from ensure import ensure_annotations
 from box import ConfigBox
 from pathlib import Path
 from typing import Any
-
-
+import os
+from box.exceptions import BoxValueError
+import yaml
+from src.wine.logging import logger
 
 @ensure_annotations
 def read_yaml(path_to_yaml: Path) -> ConfigBox:
